f0217af1aa0c36c3056e2b4ca5387042f7c2f8a7
bipin-y-lab <bipinyadav007700@gmail.com>
2022-12-01 19:28:28 +0100

    Pushing the changes

--- let_it_snow.py
+++ let_it_snow.py
@@ -1,12 +1,13 @@
 import turtle
 import numpy as np
-
+import random
 
 def main(speed=0, bg_color="grey"):
     # create Turtle object
     turtle_screen = turtle.Screen()
     myTurtle = turtle.Turtle()
-    
+    turtle.colormode(255)
+
     # set speed to 'fastest = 0'
     myTurtle.speed(speed)
     # change background color
@@ -16,13 +17,18 @@ def main(speed=0, bg_color="grey"):
     """TODO: define different colors here"""
 
 
+
     for _ in range(10):
         # define some params
         size = 18
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
-
+        r = np.random.randint(0, 255)
+        g = np.random.randint(0, 255)
+        b = np.random.randint(0, 255)
+        rgb = [r, g, b]
+        myTurtle.color(rgb)
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
