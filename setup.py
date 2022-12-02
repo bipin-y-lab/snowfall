583be5ca61f234fa54f6ee6d122bd2d787dae260
bipin-y-lab <bipinyadav007700@gmail.com>
2022-12-02 11:39:23 +0100

    setup.py modified

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['numpy', 'turtle'],
+    install_requires=['numpy', 'turtle', 'snowflake'],
     keywords=['python', 'snow', 'snowfall', 'colour'],
     classifiers=[
         "Development Status :: 1 - Planning",
