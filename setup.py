dc0da71bbcd237c092bac665078828db3d4eac5b
bipin-y-lab <bipinyadav007700@gmail.com>
2022-12-03 20:02:27 +0100

    modified

--- setup.py
+++ setup.py
@@ -1,30 +1,9 @@
-from setuptools import setup, find_packages
-import os
+from distutils.core import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-VERSION = '0.0.1'
-DESCRIPTION = 'Multiple colour snowfall'
-LONG_DESCRIPTION = 'Displaying snowfall in different colours'
-
-# Setting up
 setup(
-    name="snowflake",
-    version=VERSION,
-    author="Bipin Yadav",
-    author_email="bipin.yadav@fau.de",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    install_requires=['numpy', 'turtle'],
-    keywords=['python', 'snow', 'snowfall', 'colour'],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: DSSS professor",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+    name='snowflake',
+    version='0.1',
+    packages=['numpy', 'turtle'],
+    license='Apache License',
+    long_description=open('README.md').read(),
 )
