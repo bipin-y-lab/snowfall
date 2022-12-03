949c5a82ad25a3ee2008affa44a0fd55916eaf06
bipin-y-lab <bipinyadav007700@gmail.com>
2022-12-03 19:56:31 +0100

    modified

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, find_packages
-import codecs
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Multiple colour snowfall'
@@ -19,7 +15,7 @@ setup(
     author_email="bipin.yadav@fau.de",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['numpy', 'turtle'],
     keywords=['python', 'snow', 'snowfall', 'colour'],
